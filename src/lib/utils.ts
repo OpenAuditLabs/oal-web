c9626b07c88682f5c30bf6f83b9edcd168c6e43e
Miles Corden <cordenmiles@gmail.com>
2025-08-19 02:32:15 +0600

    feat: Integrate Prisma Client and add schema definition for database management (#56)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { PrismaClient } from '@prisma/client'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
 }
