0bf2699dcb58fbe9a7f6a2afdab821af0a9e133d
Miles Corden <cordenmiles@gmail.com>
2025-08-27 22:40:28 -0600

    feat: Enhance search functionality in ActiveAuditClient and AuditsClient with status filtering

--- src/components/audits/AuditsClient.tsx
+++ src/components/audits/AuditsClient.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { AuditStatusCard, ActiveAuditCount } from "@/components/audits";
 import { type AuditCard } from "@/actions/activities";
 
-
 interface AuditsClientProps {
   initialAudits: AuditCard[];
   searchQuery?: string;
