1bb25b50aeef536c1cff99da6a922f2fa309808e
Diego Noah <diego.noah42@gmail.com>
2025-11-05 17:18:04 -0700

    feat: Add loading state to button component (#178)
    * feat: Add loading state to button component
    
    * Fix: Correctly merge disabled state in button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -40,6 +40,7 @@ const Button = React.forwardRef<
     VariantProps<typeof buttonVariants> & {
       asChild?: boolean
       ariaLabel?: string
+      isLoading?: boolean
     }
 >(
   (
@@ -50,6 +51,7 @@ const Button = React.forwardRef<
       asChild = false,
       ariaLabel,
       children,
+      isLoading = false,
       ...props
     },
     ref
@@ -62,8 +64,32 @@ const Button = React.forwardRef<
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         aria-label={ariaLabel}
+        disabled={isLoading || props.disabled}
+        aria-busy={isLoading}
         {...props}
       >
+        {isLoading && (
+          <svg
+            className="h-4 w-4 animate-spin"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        )}
         {children}
       </Comp>
     )
