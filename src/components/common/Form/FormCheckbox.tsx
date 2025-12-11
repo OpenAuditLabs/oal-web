5dcd6e393c6c23fbf79cab9e47761c08621f89a3
Ross Williamson <rosswilliamson166@gmail.com>
2025-12-11 02:50:04 +0000

    Fix: Correct checkbox indeterminate state for accessibility

--- src/components/common/Form/FormCheckbox.tsx
+++ src/components/common/Form/FormCheckbox.tsx
@@ -24,7 +24,11 @@ export function FormCheckbox<T extends FieldValues>({
       render={({ field, fieldState }) => (
         <FormItem className='flex flex-row items-start space-x-2 rounded-md'>
           <FormControl>
-            <Checkbox checked={field.value} onCheckedChange={field.onChange} disabled={field.disabled} indeterminate={indeterminate} />
+            <Checkbox
+              checked={indeterminate ? 'indeterminate' : field.value}
+              onCheckedChange={field.onChange}
+              disabled={field.disabled}
+            />
           </FormControl>
           <div className='space-y-1 leading-none'>
             <FormLabel>
